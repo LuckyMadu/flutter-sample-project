5858451ed92f498dc853dc46736d9f694bd369e7
Lahiru Amaratunge <lahiru.a@keeneye.solutions>
2022-02-08 23:58:06 +0530

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
 main() {
-  runApp(const MyApp());
+  //runApp(const MyApp());
+  runApp(const MyAppTwo());
 }
 
 class MyApp extends StatelessWidget {
@@ -11,6 +12,26 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       home: Text('Hello world'),
+    );
+  }
+}
+
+class MyAppTwo extends StatelessWidget {
+  const MyAppTwo({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('My App'),
+        ),
+        body: const Center(
+          child: Text('Application Body', 
+          style: TextStyle(fontSize: 24.0),
+          ),
+        ),
+      ),
     );
   }
 }
