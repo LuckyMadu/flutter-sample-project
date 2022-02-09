c374b2af33433fb5b98fe1026b05864079d74f80
Lahiru Amaratunge <lahiru.a@keeneye.solutions>
2022-02-09 23:45:17 +0530

    row column widgets

--- lib/main.dart
+++ lib/main.dart
@@ -25,8 +25,66 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Flutter Title')
       ),
+      // body: Container(
+      //   color: Colors.red,
+      //   width: 300.0,
+      //   child: Column(
+      //     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      //     crossAxisAlignment: CrossAxisAlignment.center,
+      //     children: const <Widget>[
+      //       Text(
+      //         'Text 01', 
+      //         style: TextStyle(fontSize: 20.0)
+      //       ),
+      //       Text(
+      //         'Text 02', 
+      //         style: TextStyle(fontSize: 20.0)
+      //       ),
+      //       Text(
+      //         'Text 03', 
+      //         style: TextStyle(fontSize: 20.0)
+      //       ),
+      //     ],
+      //   ),
+      // ),
       body: Container(
-        color: Colors.pink,
+        color: Colors.red,
+        height: 300.0,
+        child: Row(
+         // mainAxisAlignment: MainAxisAlignment.center,
+          //crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Text 01', 
+              style: TextStyle(fontSize: 20.0)
+            ),
+            const Text(
+              'Text 02', 
+              style: TextStyle(fontSize: 20.0)
+            ),
+            const Text(
+              'Text 03', 
+              style: TextStyle(fontSize: 20.0)
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: const <Widget>[
+                 Text(
+              'Text 01', 
+              style: TextStyle(fontSize: 20.0)
+            ),
+            Text(
+              'Text 02', 
+              style: TextStyle(fontSize: 20.0)
+            ),
+            Text(
+              'Text 03', 
+              style: TextStyle(fontSize: 20.0)
+            ),
+              ]
+            ),
+          ],
+        ),
       ),
     );
   }
