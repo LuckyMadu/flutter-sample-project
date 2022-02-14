59d8aeb86f5dacc542278c4cb2921d6918d5a817
Lahiru Amaratunge <lahiru.a@keeneye.solutions>
2022-02-14 22:57:22 +0530

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 main() {
   runApp(const MyApp());
-  
 }
 
 class MyApp extends StatelessWidget {
@@ -17,36 +16,82 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+class HomePage extends StatefulWidget {
+  const HomePage({ Key? key }) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  int count = 0;
+
+  void increment() {
+    //setstate runs the build method again and again when making changes(re-draw the screen)
+    setState(() {
+      count = count + 1;
+      print(count);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Stateful/Stateless Title')
-      ),
+      appBar: AppBar(title: const Text('Stateful/Stateless Title')),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button times', 
-              style: TextStyle(
-              fontSize: 24.0,
-              )
-            ),
-            Text(
-              '1', 
-              style: Theme.of(context).textTheme.displayMedium
-            ),
+            const Text('You have pushed the button times',
+                style: TextStyle(
+                  fontSize: 24.0,
+                )),
+            Text('$count', style: Theme.of(context).textTheme.displayMedium),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: (){},
+        onPressed: increment,
         child: const Icon(Icons.add),
       ),
     );
   }
 }
+
+// ignore: must_be_immutable
+// class HomePage extends StatelessWidget {
+//   HomePage({Key? key}) : super(key: key);
+
+//   int count = 0;
+
+//   void increment() {
+//     count = count + 1;
+//     print(count);
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(title: const Text('Stateful/Stateless Title')),
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: <Widget>[
+//             const Text('You have pushed the button times',
+//                 style: TextStyle(
+//                   fontSize: 24.0,
+//                 )),
+//             Text('$count', style: Theme.of(context).textTheme.displayMedium),
+//           ],
+//         ),
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: increment,
+//         child: const Icon(Icons.add),
+//       ),
+//     );
+//   }
+// }
+
+
